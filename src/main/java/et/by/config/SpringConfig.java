ab1aa9d21188e189fae69942df9d25a9ee1607ae
NiCHUY <romannichiporukk@gmail.com>
2024-06-22 19:08:53 +0300

    Small Update
    - Added css file support.

--- src/main/java/et/by/config/SpringConfig.java
+++ src/main/java/et/by/config/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -48,5 +49,10 @@ public class SpringConfig  implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:static/css/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:static/images/");
     }
 }
