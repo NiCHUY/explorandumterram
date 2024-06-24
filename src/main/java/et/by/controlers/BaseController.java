a574c596c24b574dd13a54358d21836cb55d7357
RomanNichiporuk <romannichiporukk@gmail.com>
2024-06-25 00:22:08 +0300

    Small Update
    - Added social networks links.
    - Added new pages.
    - Played with design a little bit.

--- src/main/java/et/by/controlers/BaseController.java
+++ src/main/java/et/by/controlers/BaseController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BaseController {
     @GetMapping("/registration")
-    public String sayHello() {
+    public String registrationController() {
         return "registration";
+    }
+
+    @GetMapping("/login")
+    public String loginController() {
+        return "login";
+    }
+
+    @GetMapping("/lobby")
+    public String lobbyController() {
+        return "lobby";
     }
 }
