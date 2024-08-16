9e5ef5052bacd36b00ae290a71ced16fc67187cc
RomanNichiporuk <romannichiporukk@gmail.com>
2024-08-16 15:17:53 +0300

    Changelog 0.0.2f
    - Added user loggin cheker
    - Added notes for learning

--- src/main/java/et/by/config/SpringConfig.java
+++ src/main/java/et/by/config/SpringConfig.java
@@ -1,12 +1,10 @@
 package et.by.config;
 
-import et.by.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -52,7 +50,11 @@ public class SpringConfig  implements WebMvcConfigurer {
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:static/css/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:static/images/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:static/css/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:static/images/lobby_tiles/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:static/js/");
     }
 }
